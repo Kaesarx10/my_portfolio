ee3a3effb25b8600892b5e53e78f2714333bb465
kaesarx10 <venzor.ca@gmail.com>
2021-12-12 00:52:53 -0600

    heroku updates

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's(nxq^*lpv8%uk0e-p^zuam9tt=t@+ob44pj_)ro@q9=*73j+i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
